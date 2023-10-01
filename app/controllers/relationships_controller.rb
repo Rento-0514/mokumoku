f914e40a437ef7e960fda44e222739e8af341ffc
Rento-0514 <purin.0514@icloud.com>
2023-10-01 12:08:29 +0900

    フォローボタンajax化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,18 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
     relationship = current_user.active_relationships.find(params[:id])
     current_user.unfollow(relationship.followed)
-    redirect_to relationship.followed
+    respond_to do |format|
+      format.html { redirect_to relationship.followed }
+      format.js
+    end
   end
 end
